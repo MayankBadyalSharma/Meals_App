4d571d488bafb230ead33d0d63613a88d2ff743e
MayankBadyalSharma <mayank.sharmaaim@gmail.com>
2024-12-15 19:37:14 +0530

    First Commit

---
+++ lib/Models/category.dart
@@ -0,0 +1,15 @@
+// model class with variables and structre of app
+
+import 'package:flutter/material.dart';
+
+class Category {
+  const Category({
+    required this.id,
+    required this.title,
+    this.color = Colors.orange,
+  });
+
+  final String id;
+  final String title;
+  final Color color;
+}
---
+++ lib/Screens/meal_details.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals/Models/meal.dart';
+import 'package:meals/providers/favourites_provider.dart';
+
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
+    return Scaffold(
+        appBar: AppBar(title: Text(meal.title), actions: [
+          IconButton(
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier) // this statement gives us access to favoritemealsProvider class we've created
+
+                  .toggleMealFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+                ),
+              );
+            },
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),  // impicit animation
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  child: child,);
+              },
+               child :  Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),),
+            )
+          )
+        ]),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Hero(
+                tag: meal.id, // this is the same tag we've defined in meal_item.dart file to create animations between screens [inter screen animations]
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 14),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final ingredient in meal.ingredients)  // ingredient variable is getting values here from meal.ingredients list with the help of for each loop
+
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                ),
+              const SizedBox(height: 24),
+              Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final step in meal.steps) // step variable is getting values here from meal.steps list with the help of for each loop
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  child: Text(
+                    step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        ));
+  }
+}
